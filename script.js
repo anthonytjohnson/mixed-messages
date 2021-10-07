ee411e3cf21f7258b5290f8078567af77ca962ed
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-10-07 13:43:33 +0900

    added multiple quotes, added new quote button

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// var faker = require('faker');
+const message = document.querySelector("#message");
+const author = document.querySelector("#author");
 
-// var randomName = faker.name.findName(); // Rowan Nikolaus
-// var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-// var randomCard = faker.helpers.createCard(); // random contact card containing many properties
-
-// console.log(randomName);
 quotes = [];
 const newQuote = (quote, author) => {
   quotes.push({
@@ -13,5 +9,15 @@ const newQuote = (quote, author) => {
   });
 }
 newQuote('Spread love everywhere you go. Let no one ever come to you without leaving happier.', 'Mother Teresa');
+newQuote('When you reach the end of your rope, tie a knot in it and hang on.', 'Franklin D. Roosevelt');
+newQuote('Always remember that you are absolutely unique. Just like everyone else.', 'Margaret Mead');
+newQuote("Don't judge each day by the harvest you reap but by the seeds that you plant.", 'Robert Louis Stevenson');
+newQuote('The future belongs to those who believe in the beauty of their dreams.', 'Eleanor Roosevelt');
+newQuote('ell me and I forget. Teach me and I remember. Involve me and I learn.', 'Benjamin Franklin');
+newQuote('The best and most beautiful things in the world cannot be seen or even touched — they must be felt with the heart.', 'Helen Keller');
+newQuote('It is during our darkest moments that we must focus to see the light.', 'Aristotle');
+newQuote('Whoever is happy will make others happy too.', 'Anne Frank');
+newQuote('Do not go where the path may lead, go instead where there is no path and leave a trail.', 'Ralph Waldo Emerson');
 
-console.log(quotes[0].quote);
+message.innerText = quotes[Math.floor(Math.random()*quotes.length)].quote;
+author.innerText = `- ${quotes[Math.floor(Math.random()*quotes.length)].author}`;
